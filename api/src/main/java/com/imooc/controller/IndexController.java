a4b7f28f95cd8b5e2a5fbdbc989b38066f00e6c6
SIYUAN XU <xsiyuan@gmail.com>
2022-01-11 09:52:37 +0800

    Redis 集成 轮播图改造（redis） 商品分类及子分类改造（redis）

--- api/src/main/java/com/imooc/controller/IndexController.java
+++ api/src/main/java/com/imooc/controller/IndexController.java
@@ -8,10 +8,14 @@ import com.imooc.pojo.vo.NewItemsVo;
 import com.imooc.service.CarouselService;
 import com.imooc.service.CategoryService;
 import com.imooc.utils.IMOOCJSONResult;
+import com.imooc.utils.JsonUtils;
+import com.imooc.utils.RedisOperator;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,18 +31,40 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
   @Autowired private CarouselService carouselService;
   @Autowired private CategoryService categoryService;
+  @Autowired private RedisOperator redisOperator;
 
   @ApiOperation(value = "获取首页轮播图列表", notes = "获取首页轮播图列表", httpMethod = "GET")
   @GetMapping("/carousel")
   public IMOOCJSONResult carousel() {
-    List<Carousel> list = carouselService.queryAll(YesOrNo.YES.type);
+    /*
+     * 1. 后台运营系统，一旦广告（轮播图）发生更改，就可以删除缓存，然后重置
+     * 2. 定时重置，比如每天凌晨三点重置
+     * 3. 每个轮播图都有可能是一个广告，每个广告都会有一个过期时间，过期了，再重置
+     */
+    List<Carousel> list = new ArrayList<>();
+    String carouselStr = redisOperator.get("carousel");
+    if (StringUtils.isBlank(carouselStr)) {
+      list = carouselService.queryAll(YesOrNo.YES.type);
+      redisOperator.set("carousel", JsonUtils.objectToJson(list));
+    }
+    if (StringUtils.isNotBlank(carouselStr)) {
+      list = JsonUtils.jsonToList(carouselStr, Carousel.class);
+    }
     return IMOOCJSONResult.ok(list);
   }
 
   @ApiOperation(value = "获取一级商品分类", notes = "获取一级商品分类", httpMethod = "GET")
   @GetMapping("/category")
   public IMOOCJSONResult category() {
-    List<Category> list = categoryService.queryAllRootLevelCat();
+    List<Category> list = new ArrayList<>();
+    String categoryStr = redisOperator.get("category");
+    if (StringUtils.isBlank(categoryStr)) {
+      list = categoryService.queryAllRootLevelCat();
+      redisOperator.set("category", JsonUtils.objectToJson(list));
+    }
+    if (StringUtils.isNotBlank(categoryStr)) {
+      list = JsonUtils.jsonToList(categoryStr, Category.class);
+    }
     return IMOOCJSONResult.ok(list);
   }
 
@@ -50,7 +76,27 @@ public class IndexController {
     if (rootCatId == null) {
       return IMOOCJSONResult.errorMsg("列表不存在");
     }
-    List<CategoryVo> list = categoryService.getSubCatList(rootCatId);
+    List<CategoryVo> list = new ArrayList<>();
+    String subCatStr = redisOperator.get("subcat:" + rootCatId);
+    if (StringUtils.isBlank(subCatStr)) {
+      list = categoryService.getSubCatList(rootCatId);
+      /*
+       * 查询的key在redis中不存在，
+       * 对应的id在数据库也不存在，
+       * 此时被非法用户进行攻击，大量的请求会直接打在db上，
+       * 造成宕机，从而影响整个系统，
+       * 这种现象称之为缓存穿透。
+       * 解决方案：把空的数据也缓存起来，比如空字符串，空对象，空数组或list
+       */
+      if (list != null && list.size() > 0) {
+        redisOperator.set("subcat:" + rootCatId, JsonUtils.objectToJson(list));
+      } else {
+        redisOperator.set("subcat:" + rootCatId, JsonUtils.objectToJson(list), 5 * 60);
+      }
+    }
+    if (StringUtils.isNotBlank(subCatStr)) {
+      list = JsonUtils.jsonToList(subCatStr, CategoryVo.class);
+    }
     return IMOOCJSONResult.ok(list);
   }
 
